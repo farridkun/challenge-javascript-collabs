3e42780a125b2c4e149bbde584161c1b537c574f
farridkun <farridkuntoro@mahasiswa.unikom.ac.id>
2020-10-14 14:33:07 +0700

    Adding Custom & Initial Attr Main 🎇

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// !Informations Attributes
+// ?dob = Date Of Birth
+// ?sid = Student ID
+
+class Student {
+    constructor(name, age, dob, gender, sid, hobbies) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.gender = gender,
+        this.sid = sid;
+        this.hobbies = hobbies;
+    }
+}

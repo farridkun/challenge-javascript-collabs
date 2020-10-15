8a937c5cbd2e3f3a5d8d0925fb2a23f8f9a00c03
farridkun <farridkuntoro@mahasiswa.unikom.ac.id>
2020-10-16 01:29:33 +0700

    Adding UI with Custom css 🎈🎈

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,16 @@
 // ?sid = Student ID
 
 class Student {
-    constructor(name, age, dob, gender, sid, hobbies) {
-        this.name = name;
-        this.age = age;
-        this.dob = dob;
-        this.gender = gender,
-        this.sid = sid;
-        this.hobbies = hobbies;
-    }
+  constructor(name, age, dob, gender, sid, hobbies) {
+    this.name = name;
+    this.age = age;
+    this.dob = dob;
+    this.gender = gender;
+    this.sid = sid;
+    this.hobbies = hobbies;
+  }
+
+  speak() {
+    this.now = 'harder';
+  }
 }
